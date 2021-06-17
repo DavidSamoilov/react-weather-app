44682c64a91de6d1c6b9056ed27fe6950f1cf574
admin <dada7117@gmail.com>
2021-06-17 16:34:40 +0300

    V1 of working app, app working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
 
-
+const api = {
+  key : "022537c35f0fd09bb58056a38ce258b1",
+  base : "https://api.openweathermap.org/data/2.5/"
+}
 function  App() {
+  const [query, setQuery] = useState("")
+  const [weather, setWeather] = useState({})
+
+  const search = e => {
+    if (e.key === "Enter"){
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then (result => {
+        setWeather(result)
+        setQuery("")
+        console.log(result);
+      })
+    }
+  }
+
   const dateBuilder = d => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -21,23 +40,30 @@ function  App() {
               type="text"
               className="search-bar"
               placeholder="Search..."
-              >
+              onChange={e=>setQuery(e.target.value)}
+              value ={query}
+              onKeyPress={search}
+              />
 
-              </input>
 
           </div>
+          {(typeof weather.main != "undefined") ?(
+            <div>
+        
           <div className="location-box">
-            <div className="location">New york city, US</div>
+            <div className="location">{weather.name}, {weather.sys.country}</div>
             <div className="date">{dateBuilder(new Date())}</div>
           </div>
           <div className="weather-box">
             <div className="temp">
-              15 c
+            {Math.round(weather.main.temp)}*c
             </div>
             <div className="weather">
-              Sunny
+              {weather.weather[0].main}
             </div>
           </div>
+          </div>
+          ) : ('')}
 
         </main>
 
