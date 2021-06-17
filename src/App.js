1844130b9a5ef82fe716e5043b5e0ccfc64432e7
admin <dada7117@gmail.com>
2021-06-17 15:55:01 +0300

    getDate function added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 
 
 function  App() {
+  const dateBuilder = d => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()]
+    let date = d.getDate();
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="App">
         <main>
@@ -13,7 +25,10 @@ function  App() {
 
               </input>
 
-
+          </div>
+          <div className="location-box">
+            <div className="location">New york city, US</div>
+            <div className="date">{dateBuilder(new Date())}</div>
           </div>
 
         </main>
